c1ec8cca940f668034792241f42d6c0e91c6699a
Daniele <daniele@danrizzo.dev>
2020-06-08 16:55:39 +0200

    inserimento necrologio

--- inseriscinecrologio.php
+++ inseriscinecrologio.php
@@ -5,10 +5,10 @@
             <form method="post" action="#inserisci" enctype="multipart/form-data">
                 <input type="text" id="nome" name="nome" placeholder="Nome"><br>
                 <input type="text" id="cognome" name="cognome" placeholder="Cognome"><br>
-                <input type="text" id="eta" name="eta" placeholder="Et&agrave;"><br>
+                <input type="number" id="eta" name="eta" placeholder="Et&agrave;"><br>
                 <input type="text" id="luogo_residenza" name="luogo_residenza" placeholder="Citt&agrave; di residenza"><br>
                 <input type="text" id="luogo_celebrazione" name="luogo_celebrazione" placeholder="Chiesa celebrazione"><br>
-                <input type="date" id="data_celebrazione" name="data_celebrazione"><br>
+                <input type="date" id="data_celebrazione" name="data_celebrazione"> <input type="number" id="ora" name="ora" placeholder="00" max="23" min="0">:<input type="number" id="minuti" name="minuti" placeholder="00" max="59" min="0"><br>
                 <input type="text" id="luogo_riposo" name="luogo_riposo" placeholder="Luogo di riposo"><br>
                 <textarea name="necrologio" id="necrologio" placeholder="Necrologio"></textarea><br>
                 <input type="file" name="foto" id="foto"><br>
@@ -16,8 +16,27 @@
             </form>
         <?php
     }elseif ($_SERVER['REQUEST_METHOD']=="POST"){
-        print_r($_POST);
-        echo "<br>";
-        echo "foto in base64: ".base64_encode(file_get_contents($_FILES['foto']['tmp_name']));
+        $foto = base64_encode(file_get_contents($_FILES['foto']['tmp_name']));
+        $nome = $_POST['nome'];
+        $cognome = $_POST['cognome'];
+        $luogoResidenza = $_POST['luogo_residenza'];
+        $luogoCelebrazione = $_POST['luogo_celebrazione'];
+        $dataCelebrazione = $_POST['data_celebrazione'];
+        $luogoRiposo = $_POST['luogo_riposo'];
+        $eta = $_POST['eta'];
+        $necrologio = $_POST['necrologio'];
+        $ora = $_POST['ora'];
+        $minuti = $_POST['minuti'];
+
+        echo "Nome: $nome<br>";
+        echo "Cognome: $cognome<br>";
+        echo "Et&agrave: $nome<br>";
+        echo "Citt&agrave di residenza: $nome<br>";
+        echo "Chiesa celebrazione: $nome<br>";
+        echo "Data celebrazione: $nome<br>";
+        echo "Luogo di riposo: $nome<br>";
+        echo "Necrologio: $nome<br>";
+        echo "Foto: <br>".base64_decode($foto)."<br>";
+
 
     }
