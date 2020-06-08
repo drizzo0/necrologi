ef19dbc747b50c33298d9a16830c00231c727abc
Daniele <daniele@danrizzo.dev>
2020-06-08 17:42:41 +0200

    added insert new necrologio into db first raw view of necrologi

--- inseriscinecrologio.php
+++ inseriscinecrologio.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * TODO: Sistemare inserito da in query
+ * TODO: abilitare la pagina solo per chi è loggato
+ * TODO: sistemare il "se request method è post" mettendo la condizione "isset" sul tasto submit
+ */
     include 'dbconnect.php';
     if($_SERVER['REQUEST_METHOD']=="GET"){
         ?>
@@ -7,6 +12,7 @@
                 <input type="text" id="cognome" name="cognome" placeholder="Cognome"><br>
                 <input type="number" id="eta" name="eta" placeholder="Et&agrave;"><br>
                 <input type="text" id="luogo_residenza" name="luogo_residenza" placeholder="Citt&agrave; di residenza"><br>
+                <input type="text" id="citta_celebrazione" name="citta_celebrazione" placeholder="Citt&agrave; celebrazione"><br>
                 <input type="text" id="luogo_celebrazione" name="luogo_celebrazione" placeholder="Chiesa celebrazione"><br>
                 <input type="date" id="data_celebrazione" name="data_celebrazione"> <input type="number" id="ora" name="ora" placeholder="00" max="23" min="0">:<input type="number" id="minuti" name="minuti" placeholder="00" max="59" min="0"><br>
                 <input type="text" id="luogo_riposo" name="luogo_riposo" placeholder="Luogo di riposo"><br>
@@ -21,6 +27,7 @@
         $nome = $_POST['nome'];
         $cognome = $_POST['cognome'];
         $luogoResidenza = $_POST['luogo_residenza'];
+        $cittaCelebrazione = $_POST['citta_celebrazione'];
         $luogoCelebrazione = $_POST['luogo_celebrazione'];
         $dataCelebrazione = $_POST['data_celebrazione'];
         $luogoRiposo = $_POST['luogo_riposo'];
@@ -39,6 +46,8 @@
         echo "Necrologio: $necrologio<br>";
         echo "Tipo Mime Foto: $tipoMimeFoto<br>";
         echo "Foto: <br><img src='data: ".$tipoMimeFoto.";base64,".$foto."'/><br>";
-
+        $dataInserimento = date('Y-m-d');
+        $dataCelebrazioneCompleta = $dataCelebrazione.' '.$ora.':'.$minuti.':00';
+        $sql->query("INSERT INTO necrologi (nome, cognome, inseritoDa, dataInserimento, foto, eta, luogoResidenza, nercologio, luogoCelebrazione, dataCelebrazione, cittaCelebrazione, luogoRiposo) VALUES ('$nome', '$cognome', 1, '$dataInserimento', '$foto', '$eta', '$luogoResidenza', '$necrologio', '$luogoCelebrazione', '$dataCelebrazioneCompleta', '$cittaCelebrazione', '$luogoRiposo')");
 
     }
--- necrologi.php
+++ necrologi.php
@@ -1,2 +1,6 @@
 <?php
     include 'dbconnect.php';
+    $necrologi = $sql->query("SELECT * FROM necrologi");
+    while ($necrologio = $necrologi->fetch_assoc()){
+        print_r($necrologio);
+    }
