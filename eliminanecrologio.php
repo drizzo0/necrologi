36bb021b1423c4f97f12a0d44c8e3e8bc8e3a437
Daniele <daniele@danrizzo.dev>
2020-06-09 09:18:36 +0200

    third raw view of necrologi

--- eliminanecrologio.php
+++ eliminanecrologio.php
@@ -1,2 +1,14 @@
 <?php
     include 'dbconnect.php';
+    if(!isset($_SESSION['user']) && !isset($_SESSION['user']['username'])){
+        header('location: accedi.php');
+    }
+    if(isset($_GET['idNecrologio'])){
+        $idNecrologio = $_GET['idNecrologio'];
+        $sql->query("DELETE FROM necrologi WHERE idNecrologio='$idNecrologio'");
+        if($sql->error){
+            die($sql->error);
+        }else{
+            header('location: necrologiadmin.php');
+        }
+    }
--- necrologiadmin.php
+++ necrologiadmin.php
@@ -1,1 +1,5 @@
 <?php
+    include 'dbconnect.php';
+    if(!isset($_SESSION['user']) && !isset($_SESSION['user']['username'])){
+        header('location: accedi.php');
+    }
